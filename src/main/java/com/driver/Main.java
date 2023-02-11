c1c0b96af0e81c161e49641a1bb3559f170e67e5
Shoaib Akhtar <sk370008@gmail.com>
2023-02-11 22:49:10 +0530

    Fifth Attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,11 +2,14 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        F1 mercedes = new F1("mercedes", false);
-        mercedes.move(40, 0);
-        mercedes.accelerate(20);
-        mercedes.accelerate(-60);
+//        F1 mercedes = new F1("mercedes", false);
+//        mercedes.move(40, 0);
+//        mercedes.accelerate(20);
+//        mercedes.accelerate(-60);
 
 
+        Car c1 = new Car("Udta teer",4,2,10,false,"F23",2);
+        c1.changeGear(3);
+        c1.changeSpeed(-50,50);
     }
 }
